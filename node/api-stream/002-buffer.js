17643164afbcb86927db0cf073c09b93a5848113
TonyTang <479845854@qq.com>
2019-08-02 12:33:56 +0800

    fix

--- node/api-stream/002-buffer.js
+++ node/api-stream/002-buffer.js
@@ -1,10 +1,7 @@
-const buf = Buffer.from('hello world', 'ascii');
-console.log(buf);
-console.log(buf.toString('ascii'));
+const buf = Buffer.from('hello world', 'ascii')
+console.log(buf)
+console.log(buf.toString('ascii'))
 
-const buf1 = Buffer.alloc(10, 0x68);
-console.log(buf1);
-console.log(buf1.toString('ascii'));
-
-
-
+const buf1 = Buffer.alloc(10, 0x68)
+console.log(buf1)
+console.log(buf1.toString('ascii'))
